6ceff6149d402852f9818a6419ef10d35ee94d25
Nicky <Nicky@Nicky-PC>
2013-12-22 22:35:26 -0800

    init add baidu location

--- gen/com/cycon/macaufood/Manifest.java
+++ gen/com/cycon/macaufood/Manifest.java
@@ -9,6 +9,7 @@ package com.cycon.macaufood;
 
 public final class Manifest {
     public static final class permission {
+        public static final String BAIDU_LOCATION_SERVICE="android.permission.BAIDU_LOCATION_SERVICE";
         public static final String MAPS_RECEIVE="com.cycon.macaufood.permission.MAPS_RECEIVE";
     }
 }
--- src/com/cycon/macaufood/activities/Map.java
+++ src/com/cycon/macaufood/activities/Map.java
@@ -17,6 +17,7 @@ import android.os.Process;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,15 +25,23 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.BMapManager;
+import com.baidu.mapapi.map.LocationData;
 import com.baidu.mapapi.map.MapController;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationOverlay;
+import com.baidu.mapapi.map.MyLocationOverlay.LocationMode;
 import com.baidu.platform.comapi.basestruct.GeoPoint;
 import com.cycon.macaufood.R;
 import com.cycon.macaufood.adapters.CafeSearchListAdapter;
@@ -113,6 +122,16 @@ public class Map extends SherlockFragmentActivity {
 	
 	private BMapManager mBMapMan = null;
 	private MapView mMapView = null;
+	private MapController mMapController = null;
+	private ImageButton curLocation;
+	// 定位相关
+	LocationClient mLocClient;
+	LocationData locData = null;
+	public MyLocationListenner myListener = new MyLocationListenner();
+	//定位图层
+	MyLocationOverlay myLocationOverlay = null;
+	boolean isRequest = false;//是否手动触发请求定位
+	boolean isFirstLoc = true;//是否首次定位
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -245,12 +264,53 @@ public class Map extends SherlockFragmentActivity {
 			setUpMap();
 		}
 	}
+	
+	/**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListenner implements BDLocationListener {
+    	
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+        	Log.e("LocationOverlay", "receive location");
+            if (location == null)
+                return ;
+
+        	Log.e("LocationOverlay", "receive not null location");
+            locData.latitude = location.getLatitude();
+            locData.longitude = location.getLongitude();
+            //如果不显示定位精度圈，将accuracy赋值为0即可
+            locData.accuracy = location.getRadius();
+            // 此处可以设置 locData的方向信息, 如果定位 SDK 未返回方向信息，用户可以自己实现罗盘功能添加方向信息。
+            locData.direction = location.getDerect();
+            //更新定位数据
+            myLocationOverlay.setData(locData);
+            //更新图层数据执行刷新后生效
+            mMapView.refresh();
+            //是手动触发请求或首次定位时，移动到定位点
+            if (isRequest || isFirstLoc){
+            	//移动地图到定位点
+            	Log.d("LocationOverlay", "receive location, animate to it");
+                mMapController.animateTo(new GeoPoint((int)(locData.latitude* 1e6), (int)(locData.longitude *  1e6)));
+                isRequest = false;
+//                myLocationOverlay.setLocationMode(LocationMode.FOLLOWING);
+            }
+            //首次定位完成
+            isFirstLoc = false;
+        }
+        
+        public void onReceivePoi(BDLocation poiLocation) {
+            if (poiLocation == null){
+                return ;
+            }
+        }
+    }
 
 	private void setUpMap() {
 		mMapView=(MapView)findViewById(R.id.map);  
 		mMapView.setBuiltInZoomControls(true);  
 		//设置启用内置的缩放控件  
-		MapController mMapController=mMapView.getController();  
+		mMapController=mMapView.getController();  
 		// 得到mMapView的控制权,可以用它控制和驱动平移和缩放  
 		GeoPoint point =new GeoPoint((int)(LAT_DEFAULT* 1E6),(int)(LONG_DEFAULT* 1E6));  
 		//用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)  
@@ -258,6 +318,36 @@ public class Map extends SherlockFragmentActivity {
 		mMapController.setZoom(16);//设置地图zoom级别  
 		mMapController.enableClick(true);
 		
+		 //定位初始化
+        mLocClient = new LocationClient( this );
+        locData = new LocationData();
+        mLocClient.registerLocationListener( myListener );
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);//打开gps
+        option.setCoorType("bd09ll");     //设置坐标类型
+        option.setScanSpan(10000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+        
+        //定位图层初始化
+		myLocationOverlay = new MyLocationOverlay(mMapView);
+		//设置定位数据
+	    myLocationOverlay.setData(locData);
+	    //添加定位图层
+		mMapView.getOverlays().add(myLocationOverlay);
+		myLocationOverlay.enableCompass();
+		//修改定位数据后刷新图层生效
+		mMapView.refresh();
+		
+		curLocation = (ImageButton)findViewById(R.id.curLocation);
+		curLocation.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				isRequest = true;
+		        mLocClient.requestLocation();
+			}
+		});
 		
 /*		greenBitmap = BitmapDescriptorFactory.fromResource(R.drawable.green_pin);
 		blueBitmap = BitmapDescriptorFactory.fromResource(R.drawable.blue_pin);
@@ -791,6 +881,9 @@ public class Map extends SherlockFragmentActivity {
 	                mBMapMan.destroy();  
 	                mBMapMan=null;  
 	        }  
+	        if (mLocClient != null) {
+	            mLocClient.stop();
+	        }
 	        cafeAdapter.imageLoader.cleanup();
 	        super.onDestroy();  
 	}  
