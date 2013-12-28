77214b7b2ba5cc2ce911a0e974a93d922b47c4b4
Nicky <Nicky@Nicky-PC>
2013-12-27 23:32:05 -0800

    flushed plist info and release

--- src/com/cycon/macaufood/utilities/MFConfig.java
+++ src/com/cycon/macaufood/utilities/MFConfig.java
@@ -34,7 +34,7 @@ public class MFConfig {
 	
 	public static String DEVICE_ID;
 	
-	public static String cafe_version_update = "1380093106";
+	public static String cafe_version_update = "1388214959";
 	
 	public static String memberId;
 	public static String memberName;
